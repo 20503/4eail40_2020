11bae7c3a47ebaf7457dc0697d3c01b20974a829
20503 <72922539+20503@users.noreply.github.com>
2020-10-29 18:12:35 +0100

    Update board.go

--- exercises/chess/model/board.go
+++ exercises/chess/model/board.go
@@ -2,3 +2,12 @@
 package model
 
 // Implement type Board
+type Board [8][8]piece
+
+fonc (b *Board) MouvePiece(fromX, fromY, toX, toY int){
+  if toX < 9 {
+    if toY < 9{
+     
+    }
+  }    
+}
